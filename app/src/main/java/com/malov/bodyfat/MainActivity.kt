2849b0ad26c14f1a4bfbccc9bec5deb760987da8
Maksim Malov <maksim-masia@mail.ru>
2023-06-21 20:10:15 +0500

    Finish Startpage App I am currently working on a new activity

--- app/src/main/java/com/malov/bodyfat/MainActivity.kt
+++ app/src/main/java/com/malov/bodyfat/MainActivity.kt
@@ -2,27 +2,38 @@ package com.malov.bodyfat
 
 import android.animation.Animator
 import android.annotation.SuppressLint
-import android.icu.util.ULocale.getName
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import com.malov.bodyfat.DataBase.DataBase
 
+
 class MainActivity : AppCompatActivity() {
-    private var name : String = ""
+    private var dbHelper : DataBase = DataBase(this)
+    private lateinit var prefs: SharedPreferences
 
     @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text : TextView = findViewById(R.id.textView)
+        text.visibility = GONE
+
+        prefs = this.getSharedPreferences("name", Context.MODE_APPEND)
 
         val enterName : EditText = findViewById(R.id.enterName)
         enterName.visibility = GONE
@@ -33,36 +44,46 @@ class MainActivity : AppCompatActivity() {
         val welcomeText : ImageView = findViewById(R.id.welcomeText)
         val anim = welcomeText.animate()
             .translationY(-800F)
-            .setDuration(2000)
-     //   enterName.visibility = VISIBLE
+            .setDuration(1200)
         anim.start()
+
 
         anim.setListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator) { TODO("Not yet implemented") }
             override fun onAnimationEnd(animation: Animator) {
-                if(name=="") {
+                if(prefs.getString("name", "12")==null) {
                     enterName.visibility = VISIBLE
                     btnContinue.visibility = VISIBLE
+                } else {
+                    text.visibility = VISIBLE
+                    Handler(Looper.getMainLooper()).postDelayed(
+                        {
+                            startActivity(Intent(applicationContext, activity_2::class.java))
+                            overridePendingTransition(R.anim.rotate,R.anim.rotate);
+                        },
+                        700 // value in milliseconds
+                    )
+
+
                 }
             }
             override fun onAnimationCancel(animation: Animator) { TODO("Not yet implemented") }
             override fun onAnimationRepeat(animation: Animator) { TODO("Not yet implemented") }
         })
 
-        enterName.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                name = enterName.text.toString()
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                name = enterName.text.toString()
-            }
-        })
-
-       btnContinue.setOnClickListener{
-           enterName.setText("$name+1")
-       }
-
+            enterName.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val editPref: SharedPreferences.Editor = prefs.edit()
+                    editPref.putString("name", "${enterName.text.toString()}  ")
+                    btnContinue.setOnClickListener{
+                        editPref.commit()
+                        Toast.makeText( this@MainActivity,"Привет ${prefs.getString("name", "12")}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        text.setText(prefs.getString("name","12"))
     }
 
 
---
+++ app/src/main/java/com/malov/bodyfat/activity_2.kt
@@ -0,0 +1,11 @@
+package com.malov.bodyfat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
