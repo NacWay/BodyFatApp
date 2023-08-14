6db24fa1c18590fb935fc36c69ef621cfc0b4a31
Maksim Malov <maksim-masia@mail.ru>
2023-08-14 16:36:39 +0500

    connected firebase notification

---
+++ app/src/main/java/com/malov/bodyfat/Firebase/Notification.java
@@ -0,0 +1,45 @@
+package com.malov.bodyfat.Firebase;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.malov.bodyfat.MainActivity;
+import com.malov.bodyfat.R;
+
+public class Notification  extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        sendNotification(remoteMessage.getNotification().getBody());
+
+    }
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.tablefatfemale)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.tablefatfemale))
+                .setContentTitle(this.getString(R.string.app_name))
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
+
